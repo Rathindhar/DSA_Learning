c2042128a59b415772ae1edc657ffd2f7e574811
Rathindhar <rathindhar2004@gmail.com>
2026-07-14 17:47:41 +0530

    sort an array of 0s 1s and 2s optimal

--- Arrays/Arrays_Medium/Sort_An_Array_of_0s_1s_2s.java
+++ Arrays/Arrays_Medium/Sort_An_Array_of_0s_1s_2s.java
@@ -7,7 +7,7 @@ public class Sort_An_Array_of_0s_1s_2s {
         int[] arr = {1, 0, 2, 1, 0};
         System.out.println(Arrays.toString(bruteforce(arr)));
         System.out.println(Arrays.toString(better(arr)));
-       // System.out.println(Arrays.toString(optimal(arr)));
+        System.out.println(Arrays.toString(optimal(arr)));
 
     }
     public static int[] bruteforce(int[] arr){
@@ -27,6 +27,30 @@ public class Sort_An_Array_of_0s_1s_2s {
         while(zc-- >0)arr[index++] = 0;
         while(oc-- >0)arr[index++] = 1;
         while(tc-- >0)arr[index++] = 2;
+        return arr;
+    }
+    public static int[] optimal(int[] arr){
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+        while(mid<=high){
+            if(arr[mid] == 0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1){
+                mid++;
+            }
+            else{
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
         return arr;
     }
 }
