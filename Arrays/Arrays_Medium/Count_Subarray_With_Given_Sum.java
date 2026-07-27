522154b0db60256e7c9768901fb653e42b223cdc
Rathindhar <rathindhar2004@gmail.com>
2026-07-27 18:16:16 +0530

    Count subarray with given sum

--- Arrays/Arrays_Medium/Count_Subarray_With_Given_Sum.java
+++ Arrays/Arrays_Medium/Count_Subarray_With_Given_Sum.java
@@ -1,12 +1,14 @@
 package Arrays.Arrays_Medium;
 
+import java.util.HashMap;
+
 public class Count_Subarray_With_Given_Sum {
     public static void main(String[] args) {
         int[] arr = {1,2,3};
         int k = 3;
         System.out.println(bruteforce(arr,k));
         System.out.println(better(arr,k));
-        //System.out.println(optimized(arr));
+        System.out.println(optimized(arr,k));
 
     }
     public static int bruteforce(int[] arr, int k){
@@ -29,8 +31,22 @@ public class Count_Subarray_With_Given_Sum {
             for(int j=i; j<arr.length; j++){
                 sum+=arr[j];
                 if(sum == k) cnt++;
-                else if(sum > k) break;
             }
+        }
+        return cnt;
+    }
+    public static int optimized(int[] arr, int k){
+        HashMap<Integer,Integer> hm= new HashMap<>();
+        hm.put(0,1);
+        int preSum=0;
+        int cnt = 0;
+        for(int i=0;i<arr.length;i++){
+            preSum+=arr[i];
+            int remove = preSum-k;
+            if(hm.containsKey(remove)){
+                cnt += hm.get(remove);
+            }
+            hm.put(preSum,hm.getOrDefault(preSum,0)+1);
         }
         return cnt;
     }
